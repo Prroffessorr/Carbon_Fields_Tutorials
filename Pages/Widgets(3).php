e3840c895b57db2196f4050004693ac2381e3747
Andrew <mad.scientise@gmail.com>
2020-11-09 21:56:51 +0200

    For comments

--- Pages/Widgets(3).php
+++ Pages/Widgets(3).php
@@ -11,8 +11,27 @@
 <html lang="en">
 <body>
 
-<!-- Widgets -->
-<?php dynamic_sidebar('Example'); ?>
 
+<!--Comments-->
+<?php $comments = get_comments();
+
+foreach( $comments as $comment ){ 
+
+    $test_info    = carbon_get_comment_meta( $comment->comment_ID, 'comment_test_some_text' );
+    $rating = carbon_get_comment_meta( $comment->comment_ID, 'comment_test_select' );
+ 
+    echo ' Autor: ' . $comment->comment_author . '<br>';
+    echo ' Content: ' . $comment->comment_content . '<br>';
+
+    if ( ! empty( $test_info ) ) {
+        echo ' Rating: ' . $test_info . '<br>';
+    } 
+
+    if ( ! empty( $rating ) ) {
+        echo 'Test Info: ' . $rating . '<br>';
+    } 
+   
+}
+?>
 </body>
 </html>
